1237f68317f4e9bca0a54e1f83467e0888887992
Christopher Lamm <christopher.lamm@companycam.com>
2023-12-14 09:43:49 -0500

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Christopher Lamm on 12/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
